99cb672ddf1d0bbb65a4d86f0a97f50418a87da8
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-03-15 22:40:21 -0400

    Added resetUI function to display the clear all button and filter items input if there are list items or to hide them if there are no list items

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Globally scoped variables
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
-// Add New Item function
+// Add New List Item function
 function addItem(e) {
-  e.preventDefault();
+  e.preventDefault(); // prevent the form from submmiting
 
-  const newItem = itemInput.value;
+  const newItem = itemInput.value; 
   // Validate input
   if (newItem.value === '') {
     alert('Please add an item.');
@@ -17,38 +19,64 @@ function addItem(e) {
   // Create list item & append to child
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
-  // Create button & append to child
-  const button = createButton('remove-item btn-link text-red')
+
+  // Call button function & append to child
+  const button = createButton('remove-item btn-link text-red') // classes passed as argument
   li.appendChild(button);
-  document.querySelector('.items').appendChild(li);
+
+  // Add li to the DOM
+  itemList.appendChild(li);
+  resetUI(); // called here because if there was previously an empty list, the call brings back the hidden items
   itemInput.value = ''; // clears the text input after "Add Item" button is clicked
 }
 
-
+// Create new button
 function createButton(classes) {
   const button = document.createElement('button');
-  button.className = classes;
+  button.className = classes; // assign the button elem classes = to the param of classes passed to createButton func
   const icon = createIcon('fa-solid fa-xmark');
   button.appendChild(icon);
   return button;
 }
 
+// Create new "x" icon
 function createIcon(classes) {
   const icon = document.createElement('i');
   icon.className = classes;
   return icon;
 }
 
+// Remove a single list item
 function removeItem(e) {
-  if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
-    //^click     ^button       ^li        ^all gone
+  if (e.target.parentElement.classList.contains('remove-item')) { // if parent of clicked on elem contains the class
+    if (confirm('Are you sure you want to delete this item?')) {
+      e.target.parentElement.parentElement.remove(); // e.click-on.button-Elem.li-Elem.remove()
+      resetUI();
+    }
   }
 }
 
+// Remove all items
 function clearItems() {
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild);
+  if (confirm('Are you sure you want to delete all items?')) {
+    while (itemList.firstChild) { 
+      itemList.removeChild(itemList.firstChild); 
+    } // The while loop is saying, remove the firstChild so long as there is a firstChild to remove.
+  }
+  resetUI();
+}
+
+// This function must be called everwhere a list item can be deleted or created.
+// hides the "Clear All" button UI and the "Filter Items" input UI if there are no list items, returns them when new list items are created.
+function resetUI() { 
+  const items = itemList.querySelectorAll('li');
+
+  if (items.length === 0) { // array-like
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
   }
 }
 
@@ -56,3 +84,5 @@ function clearItems() {
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+
+resetUI(); // global call
